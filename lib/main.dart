013b3a66c85d0cb26cdbaebafc4c15526fcf565b
AhmedMohammedSoliman <110429772+AhmedMohammedSoliman@users.noreply.github.com>
2023-01-26 12:52:53 +0200

    Revert "splash"

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:e_commerce_app/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,10 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: SplashScreen.routeName,
-      routes: {
-        SplashScreen.routeName : (context) => SplashScreen(),
-      },
     );
   }
 }
